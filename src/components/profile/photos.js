3bd274281c0ba36c35b2cf893ad9db12a6c65a9b
vikehieto <vikehietoyhoshii@gmail.com>
2021-09-06 15:53:07 +0530

    added component, fixed photo username.todo-> fix toggle like and list all comments

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Post from '../post/index'
 import PropTypes from 'prop-types'
 import Skeleton from 'react-loading-skeleton'
+import UserContext from '../../context/user'
+import { useContext } from 'react'
+import { getUserByUserId } from '../../services/firebase'
 
 export default function Photos( { photos } ){
 
+    const [username, setusername] = useState('')
 
+    const [popup, setPopup] = useState({
+        popup:false,
+        photo: null
+    })
+
+    const { user } = useContext(UserContext)
+
+    async function getuserinfo(){
+        let [userl] = await getUserByUserId(popup.photo.userId)
+        console.log(popup.photo)
+        setusername(userl.username)
+    }
+
+    useEffect(() => {
+        async function getData(){
+            if(popup.popup && !popup.photo.userLikedPhoto){
+                getuserinfo()
+        
+                let userLikedPhoto = false
+            
+                if(popup.photo.likes.includes(user.uid)){
+                userLikedPhoto = true
+                }        
+                setPopup({popup: true, photo: { username: username, ...popup.photo, userLikedPhoto }}
+                )
+
+            }
+        }
+        getData()
+        
+    }, [popup.photo])
+
+    function handlePopup (photo) {
+        setPopup({popup: !popup.popup,
+            photo: photo || null}
+        )
+    }
     return(
         <div className="container mx-auto h-16 border-t border-gray-primary mt-12 pt-4">
             <div className='grid grid-cols-3 gap-4 mt-4 mb-12'>
@@ -15,11 +58,10 @@ export default function Photos( { photos } ){
                 ) : (
                     photos.map(photo => (
                         <div className="relative group" key={photo.docId}>
-                            <img src={photo.imageSrc} alt={photo.caption} />   
-
+                            <img src={photo.imageSrc} alt={photo.caption}  />   
                             <div className="absolute bottom-0 left-0 bg-gray-200 
                                 z-10 w-full justify-evenly items-center h-full 
-                                bg-black-faded group-hover:flex hidden">
+                                bg-black-faded group-hover:flex hidden" onClick={() => handlePopup(photo)}>
                                 <p className='flex items-center text-white font-bold'>
                                     <svg 
                                         xmlns="https://www.w3.org/2000/svg" 
@@ -54,6 +96,32 @@ export default function Photos( { photos } ){
                         </div>
                     ))
                 ) }
+                {
+                    popup.popup ? (
+                    <div className="fixed top-0 left-0 w-screen min-h-screen max-h-full overflow-y-scroll">
+                        <div className='w-3/5 mx-auto bg-white'>
+                         <span className="text-2xl" onClick={() => handlePopup(null)}>X CLOSE</span>
+                        <Post content={popup.photo} />
+                         {/* {popup.photo.comments.map(item => 
+                              <p key={`${item.comment}-${item.displayName}`} 
+                                 className="mb-1" >
+                                 link to commenter profile
+                                 <Link to={`/p/${item.displayName}`}>
+                                     <span className="mr-1 font-bold">
+                                        {item.displayName}
+                                     </span>
+                                 </Link>
+                                 <span>
+                                     {item.comment}
+                                 </span>
+                             </p>
+                         )}*/}
+                         
+                        </div>
+
+                    </div>
+                    ) : null
+                }
             </div>
         {!photos || (photos.length === 0 && 
             <p className="text-center text-2xl">
